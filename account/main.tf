6852066199c27f2a64aab841c175e4f4e9d312d0
Diana Dauletkerey <ddauletkerey02@gmail.com>
2024-01-24 20:18:57 +0000

    maintf changes

---
+++ account/main.tf
@@ -0,0 +1,55 @@
+data "google_billing_account" "acct" {
+  display_name = "My Billing Account"
+  open = true
+}
+
+
+resource "random_string" "random" {
+  length           = 7
+  special          = false
+  override_special = "/@£$"
+  numeric = false 
+  lower = true 
+  upper = false
+}
+
+
+resource "google_project" "testproject" {
+  name = "OCT-GCP-TEAM3"
+  project_id = random_string.random.result
+  billing_account = data.google_billing_account.acct.id
+}
+
+
+resource "null_resource" "set-project" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  
+  provisioner "local-exec" {
+    command = "gcloud config set project ${google_project.testproject.project_id}"
+  }
+}
+
+
+
+resource "null_resource" "enable-apis" {
+  depends_on = [
+    google_project.testproject,
+    null_resource.set-project
+  ]
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+        gcloud services enable compute.googleapis.com
+        gcloud services enable dns.googleapis.com
+        gcloud services enable storage-api.googleapis.com
+        gcloud services enable container.googleapis.com
+        gcloud services enable file.googleapis.com
+    EOT
+  }
+}
+
