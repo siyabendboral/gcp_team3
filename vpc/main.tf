7397db0ff471fa1b4f82600fc3eaacceee3e9c7d
Diana Dauletkerey <ddauletkerey02@gmail.com>
2024-01-25 03:16:18 +0000

    new vpc name

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_network" "vpc" {
-	name = "vpc"
+resource "google_compute_network" "vpc_project" {
+	name = "vpc_project"
 	auto_create_subnetworks = "true"
 	routing_mode = "GLOBAL"
 }
