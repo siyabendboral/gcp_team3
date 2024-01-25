a9203b05e4ea04e94e2d330d70ccda46e37e90d2
Diana Dauletkerey <ddauletkerey02@gmail.com>
2024-01-25 17:19:39 +0000

    deleting

--- account/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "team3-bucket-project"
-    key    = "path/to/my/way"
-    region = "us-east-1"
-  }
-}
--- vpc/main.tf
+++
@@ -1,5 +0,0 @@
-resource "google_compute_network" "vpc_project" {
-	name = "vpc_project"
-	auto_create_subnetworks = "true"
-	routing_mode = "GLOBAL"
-}
